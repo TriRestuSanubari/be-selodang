fecfc4e09e1b395941187b16d3a50f4bb52cf018
TriRestuSanubari <restuakunkuliah.com>
2025-11-30 16:00:39 +0700

    Fix prisma path and move prisma.ts

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import { IReqUser } from "../types/auth";
-import { prisma } from "../../prisma/prisma";
+import { prisma } from "../prisma";
 
 export default {
   async index(req: IReqUser, res: Response) {
